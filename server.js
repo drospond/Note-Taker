ff8b4721cd944926ceb6f5fd1262e0eb75d1e9ca
drospond <david.rospond@gmail.com>
2020-04-02 22:10:25 -0400

    Notes post method added to server

--- server.js
+++ server.js
@@ -1,17 +1,52 @@
-var express = require("express");
-var path = require("path");
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
 
-var app = express();
-var PORT = process.env.PORT || 3000;
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-app.get("*", function(req, res) {
-    res.json(path.join(__dirname, "index.html"));
-  });
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get("/notes", function(req, res) {
-    res.json(path.join(__dirname, "notes.html"));
+    res.sendFile(path.join(__dirname, "notes.html"));
+  });
+
+app.get("/api/notes", function(req, res) {
+  fs.readFile(path.join(__dirname, "db/db.json"), "utf8", function(err, data){
+    if (err) {
+      return console.log(err);
+    }
+    console.log(data);
+    res.json(JSON.parse(data));
+  })
+});
+
+app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+  });
+  
+app.post("/api/notes", function(req, res) {
+  fs.readFile(path.join(__dirname, "db/db.json"), "utf8", function(err, data){
+    if (err) {
+      return console.log(err);
+    }
+    const notes = JSON.parse(data);
+    notes.push(req.body);
+    fs.writeFile(path.join(__dirname, "db/db.json"), JSON.stringify(notes),function(err){
+      if (err) {
+        return console.log(err);
+      }
+      console.log("Success!");
+    })
+  })
+  res.json(req.body);
+});
+
+app.delete("/api/notes/:id", function(req, res) {
+    res.json();
   });
 
 app.listen(PORT, function() {
-    console.log("App listening on http://localhost:3000/" + PORT);
+    console.log("App listening on http://localhost:" + PORT);
   });
