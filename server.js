0c10410e10616137b46efed162bdec5a14ac41cb
drospond <david.rospond@gmail.com>
2020-04-03 15:55:02 -0400

    Added uniquie id to notes

--- server.js
+++ server.js
@@ -8,45 +8,71 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "notes.html"));
-  });
+let newId = 0;
 
-app.get("/api/notes", function(req, res) {
-  fs.readFile(path.join(__dirname, "db/db.json"), "utf8", function(err, data){
+app.get("/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "notes.html"));
+});
+
+app.get("/api/notes", function (req, res) {
+  fs.readFile(path.join(__dirname, "db/db.json"), "utf8", function (err, data) {
     if (err) {
       return console.log(err);
     }
-    console.log(data);
     res.json(JSON.parse(data));
-  })
+  });
 });
 
-app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.post("/api/notes", function (req, res) {
+  fs.readFile(path.join(__dirname, "db/db.json"), "utf8", function (err, data) {
+    if (err) {
+      return console.log(err);
+    }
+    const notes = JSON.parse(data);
+    req.body.id = newId;
+    newId++;
+    notes.push(req.body);
+    fs.writeFile(
+      path.join(__dirname, "db/db.json"),
+      JSON.stringify(notes),
+      function (err) {
+        if (err) {
+          return console.log(err);
+        }
+        console.log("Success!");
+        res.json(req.body);
+      }
+    );
   });
-  
-app.post("/api/notes", function(req, res) {
-  fs.readFile(path.join(__dirname, "db/db.json"), "utf8", function(err, data){
+});
+
+app.delete("/api/notes/:id", function (req, res) {
+  fs.readFile(path.join(__dirname, "db/db.json"), "utf8", function (err, data) {
     if (err) {
       return console.log(err);
     }
     const notes = JSON.parse(data);
     notes.push(req.body);
-    fs.writeFile(path.join(__dirname, "db/db.json"), JSON.stringify(notes),function(err){
-      if (err) {
-        return console.log(err);
+    fs.writeFile(
+      path.join(__dirname, "db/db.json"),
+      JSON.stringify(notes),
+      function (err) {
+        if (err) {
+          return console.log(err);
+        }
+        console.log("Success!");
       }
-      console.log("Success!");
-    })
-  })
-  res.json(req.body);
+    );
+  });
+  res.json();
 });
 
-app.delete("/api/notes/:id", function(req, res) {
-    res.json();
-  });
+app.listen(PORT, function () {
+  console.log("App listening on http://localhost:" + PORT);
+});
 
-app.listen(PORT, function() {
-    console.log("App listening on http://localhost:" + PORT);
-  });
+//TODO: check why note text doesn't render in index.js
